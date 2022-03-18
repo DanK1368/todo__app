829355c8f0019017f264ea6e9ed0782a7a79bebe
DanK1368 <danielkoett@gmail.com>
2022-03-18 03:10:25 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# Frontend Mentor - Todo app solution
+
+This is a solution to the [Todo app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/todo-app-Su1_KokOW). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Overview
+
+This app half functional. I'm still trying to make it fully work.
+It allows for you to create,search,delete, and filter your todos.
+
+Unfortunately, the draggable feature breaks as soon as new todos are added. :(
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Add new todos to the list
+- Mark todos as complete
+- Delete todos from the list
+- Filter by all/active/complete todos
+- Clear all completed todos
+- Toggle light and dark mode
+- **Bonus**: Drag and drop to reorder items on the list
+
+### Screenshot
+
+![](assets/screenshot.png)
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- SASS with partials
+- Flexbox
+- Mobile-first workflow
+- vanilla JS
+
+### What I learned
+
+- Mobile-first approach to building the application
+
+- I haven't really worked on projects that had a light/dark theme toggle. I'm still in the process learning the best way to separate the different styles. In this case, I created two separate folders containing the SASS files. One for dark, and the other for light theme. JS is used to update the href in link tag for the stylesheet to choose between the two themes. I'm not sure if that is a valid approach.
+
+- I learned to create different functions to solve the problems of filtering the todos differently, based on the challenge.
+
+- I also integrated a search function in the input field to filter todos based on the term entered by the user. This also let me practice more on chaining different array methods together. In this case the filter and forEach method.
+
+- For the draggable feature, I followed [Web Dev Simplified](https://www.youtube.com/watch?v=jfYWwQrtzzY). It was my first time to integrate this feature, and learn how to program it. Unfortunately it seems to work only with the todos loaded on start. The code seems to break as soon as new todos are added. I'm still trying to find a solution for this to fix it.
---
+++ app.js
@@ -0,0 +1,183 @@
+const addTodo = document.querySelector(".form__add");
+const todoList = document.querySelector(".todos");
+const deleteAllButton = document.querySelector(".todos__delete-all");
+const remainingTodos = document.querySelector(".todos__remaining");
+const todosFilter = document.querySelector(".todos__filter");
+const themeToggler = document.querySelector(".checkbox");
+const themeIcon = document.querySelector(".header__icon");
+const imgMobile = document.querySelector(".header__bg-img-mobile");
+const imgDesktop = document.querySelector(".header__bg-img-desktop");
+const stylesheet = document.querySelector(".stylesheet");
+const draggables = document.querySelectorAll(".todos__item");
+
+//counting how many todos left
+const todoCount = () => {
+  const items = todoList.children.length;
+  const completedItems = Array.from(todoList.children).filter((todo) =>
+    todo.classList.contains("completed")
+  ).length;
+  const total = items - completedItems;
+  remainingTodos.textContent = `${total} items left`;
+};
+todoCount();
+
+//template for new todo
+const todoTemplate = (todo) => {
+  const html = `
+    <li class="todos__item todo-style" draggable="true">
+    <button class="todos__complete btn"></button>
+    <p class="todos__text">${todo}</p>
+
+    <button class="todos__delete">
+    <img
+      class="todos__delete__img"
+      src="assets/icon-cross.svg"
+      alt="an icon with the symbol x that will allow you to delete the selected todo "
+    />
+    </button>
+    </li>`;
+
+  todoList.innerHTML += html;
+};
+
+//generate new todo
+addTodo.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const todo = addTodo.form__add.value.trim();
+  if (todo.length) {
+    todoTemplate(todo);
+    console.log(todo);
+    addTodo.reset();
+  }
+  todoCount();
+});
+
+//delete or mark a todo as complete
+todoList.addEventListener("click", (e) => {
+  //delete a todo
+  if (e.target.classList.contains("todos__delete__img")) {
+    e.target.parentElement.parentElement.remove();
+  }
+  //mark/unmark as complete
+  if (
+    e.target.classList.contains("todos__complete") ||
+    e.target.classList.contains("todos__complete__img")
+  ) {
+    e.target.parentElement.classList.toggle("completed");
+  }
+
+  todoCount();
+});
+
+//delete all completed todos
+deleteAllButton.addEventListener("click", () => {
+  Array.from(todoList.children)
+    .filter((todo) => todo.classList.contains("completed"))
+    .forEach((todo) => todo.remove());
+
+  todoCount();
+});
+
+// filter todos function
+const filterTodos = (word) => {
+  Array.from(todoList.children)
+    .filter((todo) => !todo.textContent.toLowerCase().includes(word))
+    .forEach((todo) => todo.classList.add("filtered"));
+
+  Array.from(todoList.children)
+    .filter((todo) => todo.textContent.toLowerCase().includes(word))
+    .forEach((todo) => todo.classList.remove("filtered"));
+};
+
+//searching todos
+addTodo.addEventListener("keyup", (e) => {
+  const word = addTodo.form__add.value.trim().toLowerCase();
+
+  filterTodos(word);
+});
+
+//filter between All, Active, completed todos
+todosFilter.addEventListener("click", (e) => {
+  //display active todos
+  if (e.target.classList.contains("todos__active")) {
+    Array.from(todoList.children)
+      .filter((todo) => todo.classList.contains("completed"))
+      .forEach((todo) => todo.classList.add("filtered"));
+
+    Array.from(todoList.children)
+      .filter((todo) => !todo.classList.contains("completed"))
+      .forEach((todo) => todo.classList.remove("filtered"));
+  }
+  // display completed todos
+  else if (e.target.classList.contains("todos__completed")) {
+    Array.from(todoList.children)
+      .filter((todo) => !todo.classList.contains("completed"))
+      .forEach((todo) => todo.classList.add("filtered"));
+
+    Array.from(todoList.children)
+      .filter((todo) => todo.classList.contains("completed"))
+      .forEach((todo) => todo.classList.remove("filtered"));
+  }
+  // display all todos
+  else if (e.target.classList.contains("todos__all")) {
+    Array.from(todoList.children).forEach((todo) => {
+      todo.classList.remove("filtered");
+    });
+  }
+});
+
+//change themes
+themeToggler.addEventListener("change", (e) => {
+  if (e.target.checked) {
+    themeIcon.setAttribute("src", "assets/icon-moon.svg");
+    imgMobile.setAttribute("src", "assets/bg-mobile-light.jpg");
+    imgDesktop.setAttribute("src", "assets/bg-desktop-light.jpg");
+    stylesheet.setAttribute("href", "styles/light_mode/main.css");
+  } else {
+    themeIcon.setAttribute("src", "assets/icon-sun.svg");
+    imgMobile.setAttribute("src", "assets/bg-mobile-dark.jpg");
+    imgDesktop.setAttribute("src", "assets/bg-desktop-dark.jpg");
+    stylesheet.setAttribute("href", "styles/dark_mode/main.css");
+  }
+});
+
+//draggable todo feature
+draggables.forEach((item) => {
+  item.addEventListener("dragstart", () => {
+    item.classList.add("dragging");
+  });
+  item.addEventListener("dragend", () => {
+    item.classList.remove("dragging");
+  });
+});
+
+todoList.addEventListener("dragover", (e) => {
+  e.preventDefault();
+  const afterElement = positionDraggableElement(todoList, e.clientY);
+  const draggable = document.querySelector(".dragging");
+  if (afterElement == null) {
+    todoList.appendChild(draggable);
+  } else {
+    todoList.insertBefore(draggable, afterElement);
+  }
+});
+
+function positionDraggableElement(todoList, y) {
+  const draggableItems = [
+    ...todoList.querySelectorAll(".todos__item:not(.dragging)"),
+  ];
+
+  return draggableItems.reduce(
+    (closest, item) => {
+      const box = item.getBoundingClientRect();
+      const offset = y - box.top - box.height / 2;
+      if (offset < 0 && offset > closest.offset) {
+        return { offset: offset, element: item };
+      } else {
+        return closest;
+      }
+    },
+    { offset: Number.NEGATIVE_INFINITY }
+  ).element;
+}
